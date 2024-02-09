d976d8368e24b6b1c0fd406a3a9db5d1e1fbf52e
FluxFlu <70078024+FluxFlu@users.noreply.github.com>
2024-02-09 00:09:22 -0500

    Added manual definitions for consts
    Added manual definitions for HOST_NAME_MAX and LOGIN_NAME_MAX from
    <limits.h>, as these may not always be defined there.

--- src/util/def.c
+++ src/util/def.c
@@ -2,3 +2,12 @@
 #define charSize sizeof(char)
 #define tokenSize sizeof(Token)
 #define ptrSize sizeof(char*)
+
+
+#include <limits.h>
+#ifndef HOST_NAME_MAX
+    #define HOST_NAME_MAX 4096
+#endif
+#ifndef LOGIN_NAME_MAX
+    #define LOGIN_NAME_MAX 4096
+#endif
